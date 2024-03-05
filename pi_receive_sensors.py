1a2d7da2f0f2f5808a2985c4889f0b5413d643b9
Mir Jeffres <smjeff515@gmail.com>
2024-03-04 21:24:51 -0500

    UART consistently working with formatted sensor data from esp

--- pi_receive_sensors.py
+++ pi_receive_sensors.py
@@ -1,60 +1,57 @@
 import serial
 import time
 
+def send_to_pd(prefix, value):
+    print(prefix + ' ' + value.decode(encoding='ascii').strip())
+
 port = '/dev/ttyAMA0'
 # serial.EIGHTBITS
 esp = serial.Serial(port)
 print(esp.name)
 esp.baudrate = 9600
-for i in range(10000):
-    print(esp.read())
-    time.sleep(0.1)
-# connected = True
+connected = True
 
 #processing loop
-# while True:
+while connected:
 #     dataIn = esp.read()
 #     print(dataIn)
     #first bit of data
-    # print("read")
-#     dataIn = esp.read(1)
-#     if dataIn == "x":
-#         x-position
-#         x = esp.read_until()    #reads until /n
-#         send_to_pd("/x_pos", x)
-#     elif dataIn == "y":
-#         y-position
-#         x-position
-#         y = esp.read_until()    #reads until /n
-#         send_to_pd("/y_pos", y)
-#     elif dataIn == "z":
-#         z-position
-#         z = esp.read_until()    #reads until /n
-#         send_to_pd("/z_pos", z)
-#     elif dataIn == "H":
-#         x velocity
-#         H = esp.read_until()    #reads until /n
-#         send_to_pd("/x_vel", x)
-#     elif dataIn == "H":
-#         y velocity
-#         V = esp.read_until()    #reads until /n
-#         send_to_pd("/y_vel", V)
-#     elif dataIn == "F":
-#         z velocity
-#         F = esp.read_until()    #reads until /n
-#         send_to_pd("/z_vel", F)
-#     elif dataIn == "b":
-#         body signal
-#         b = esp.read_until()    #reads until /n
-#         send_to_pd("/body", b)
-#     elif dataIn == "~":
-#         print("~")
-#         connected = False
-#         break
-#     time.sleep(0.015)   #sleep time should be the same as the sampling period of the sensors
+    #print("read")
+    dataIn = esp.read(1)
+    #print(dataIn)
+    if dataIn == b'x':
+        #x-position
+        x = esp.read_until()    #reads until /n
+        #x = x.decode(encoding='ascii')
+        send_to_pd("/x_pos", x)
+    elif dataIn == b'y':
+        #y-position
+        y = esp.read_until()    #reads until /n
+        send_to_pd("/y_pos", y)
+    elif dataIn == b'z':
+        #z-position
+        z = esp.read_until()    #reads until /n
+        send_to_pd("/z_pos", z)
+    elif dataIn == b'H':
+        #x velocity
+        H = esp.read_until()    #reads until /n
+        send_to_pd("/x_vel", x)
+    elif dataIn == b'V':
+        #y velocity
+        V = esp.read_until()    #reads until /n
+        send_to_pd("/y_vel", V)
+    elif dataIn == b'F':
+        #z velocity
+        F = esp.read_until()    #reads until /n
+        send_to_pd("/z_vel", F)
+    elif dataIn == b'b':
+        #body signal
+        b = esp.read_until()    #reads until /n
+        send_to_pd("/body", b)
+    elif dataIn == b'~':
+        print("~")
+        connected = False
+        break
+    time.sleep(0.015)   #sleep time should be the same as the sampling period of the sensors
 
 
-
-
-# def send_to_pd(prefix, value):
-#     print(prefix + ' ' + value)
