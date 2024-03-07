af0156beface8d0eaeebe23b84b391b4f7645ef8
Mir Jeffres <smjeff515@gmail.com>
2024-03-06 21:35:14 -0500

    updating some pd stuff

--- run_pd.sh
+++ run_pd.sh
@@ -1,4 +1,4 @@
 qjackctl
 cd /home/hack/Documents/NEWMIRRORFOLDER/auditory-mirror && lxterminal -e python pi_receive_sensors.py
-pd -verbose -jack -r 44100 -outchannels 8 -noadc -nomidi -blocksize 2048 -gui /home/hack/Documents/NEWMIRRORFOLDER/auditory-mirror/jrubin_PD_Files/oscReceive.pd
+pd -verbose -jack -r 44100 -outchannels 8 -noadc -nomidi -blocksize 2048 -nogui /home/hack/Documents/NEWMIRRORFOLDER/auditory-mirror/jrubin_PD_Files/oscReceive.pd
 
