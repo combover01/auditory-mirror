2aa4844ceef0607dc624827af8cba8d2e97871c5
Mir Jeffres <smjeff515@gmail.com>
2024-03-06 21:04:12 -0500

    updated shell file and fixd output tilde

--- run_pd.sh
+++ run_pd.sh
@@ -1,1 +1,4 @@
-pd -jack -r 44100 -outchannels 8 -noadc -nomidi -blocksize 2048 -gui /home/hack/Documents/NEWMIRRORFOLDER/auditory-mirror/jrubin_PD_Files/oscReceive.pd
+qjackctl
+cd /home/hack/Documents/NEWMIRRORFOLDER/auditory-mirror && lxterminal -e python pi_receive_sensors.py
+pd -verbose -jack -r 44100 -outchannels 8 -noadc -nomidi -blocksize 2048 -gui /home/hack/Documents/NEWMIRRORFOLDER/auditory-mirror/jrubin_PD_Files/oscReceive.pd
+
